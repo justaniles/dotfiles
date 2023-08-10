e01186d9071647601210d7c73b1edf50f9b18d47
Justin Niles <justin.niles12@gmail.com>
2023-08-09 22:10:15 -0700

    Fix git-prune-untracked-branches.sh

--- scripts/git-prune-untracked-branches.sh
+++ scripts/git-prune-untracked-branches.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 git fetch -p --quiet
+old_ifs=$IFS
+IFS=$'\n'
 
 branchesWithGoneRemote=$(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}')
 branchesWithNoRemote=$(git branch --format "%(refname:short) %(upstream)" | awk '{if (!$2) print $1;}')
-branchesToPrune=$((echo $branchesWithGoneRemote; echo $branchesWithNoRemote) | tr " " "\n" | sort)
+branchesToPrune=$((echo "$branchesWithGoneRemote"; echo "$branchesWithNoRemote") | sort)
 
 branchCount=0
 branchStr="Prune below branches which have no remote?"
 for branch in $branchesToPrune; do
-  branchCount+=1
+  ((branchCount+=1))
   branchStr+=$'\n'"  - ${branch}"
 done
 
@@ -20,10 +22,13 @@ if (( $branchCount > 0 )); then
     deletedBranchCount=0
     for branch in $branchesToPrune; do
       if git branch -D $branch; then
-        deletedBranchCount+=1
+        ((deletedBranchCount+=1))
       fi
     done
+    echo "✅ Deleted $deletedBranchCount branches."
   fi
 else
   echo "No branches to prune."
 fi
+
+IFS=$old_ifs
